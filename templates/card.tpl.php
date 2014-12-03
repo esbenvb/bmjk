33c8296f10a9f299b92356b0a91db380d951f192
Esben von Buchwald <esben@vonbuchwald.dk>
2014-12-04 00:08:13 +0100

    Removed name from card template.

--- templates/card.tpl.php
+++ templates/card.tpl.php
@@ -2,5 +2,4 @@
 		<p><strong>Til:</strong> <?php print $sender_name; ?> (<?php print $sender_email_link; ?>)</p>
 		<p><strong>Fra:</strong> <?php print $recipient_name; ?> (<?php print $recipient_email_link; ?>)</p>
 	</div>
-		<p><?php print $recipient_name; ?>,</p>
-		<?php print $message_filtered; ?>
+	<?php print $message_filtered; ?>
