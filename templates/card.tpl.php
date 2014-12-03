59b1175a13da6b27338fada9387c391da08eb2f1
Esben von Buchwald <esben@vonbuchwald.dk>
2014-12-04 00:13:36 +0100

    Fixed reversed sender and recipient

--- templates/card.tpl.php
+++ templates/card.tpl.php
@@ -1,5 +1,5 @@
 	<div class="card-meta">
-		<p><strong>Til:</strong> <?php print $sender_name; ?> (<?php print $sender_email_link; ?>)</p>
-		<p><strong>Fra:</strong> <?php print $recipient_name; ?> (<?php print $recipient_email_link; ?>)</p>
+		<p><strong>Til:</strong> <?php print $recipient_name; ?> (<?php print $recipient_email_link; ?>)</p>
+		<p><strong>Fra:</strong> <?php print $sender_name; ?> (<?php print $sender_email_link; ?>)</p>
 	</div>
 	<?php print $message_filtered; ?>
