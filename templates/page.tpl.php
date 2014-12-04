2dcfea4260b3f0a225cae4ad88f02d87cbde5aa3
Bo Skakke <bo@skakke.dk>
2014-12-04 10:50:47 +0100

    ie

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -13,7 +13,7 @@
     
     <link href="css/bootstrap.css" rel="stylesheet">
    
-    <!--[if lt IE 9]>
+    <!--[if lte IE 9]>
       <link href="css/ie.css" rel="stylesheet">
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
